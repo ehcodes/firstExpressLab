4c68a7e5d4a263aafa641395efd63cfce899477e
ehcodes <engrid.hernandez@gmail.com>
2023-09-16 20:53:50 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,11 +5,17 @@ const siteAddress = `http://localhost:3000/`
 
 // Greetings
 app.get(`/greeting`,function(req,res){
-  res.send(`Hey there, you person!`)
+  res.send(`<p>Hey there, you person!</p>`)
 })
 
 app.get(`/greeting/:name`,function(req,res){
-  res.send(`I didn't expect to see you here, ${req.params.name}!`)
+  res.send(`<p>I didn't expect to see you here, ${req.params.name}!</p>`)
+})
+
+// Tip Calculator
+app.get(`/tip/:total/:tipPercentage`,function(req,res){
+  const tip = req.params.total*req.params.tipPercentage/100
+  res.send(`<p>${tip}</p>`)
 })
 
 app.listen(localPort, () => {
